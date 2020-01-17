d42d0ee3f92b4ca7e22e094e0e5b67ebc126a4f0
clarissequilapio <clairequilapio@gmail.com>
2020-01-17 19:05:00 +0800

    count 0-1

--- SevenSegmentDisplay/Form1.vb
+++ SevenSegmentDisplay/Form1.vb
@@ -1,26 +1,30 @@
 ﻿Public Class Form1
+    Private counter As Integer = 0
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        If Timer1.Interval = 1000 Then
-            btn1.BackColor = Color.Red
-            btn2.BackColor = Color.Red
+        counter += 1
+        If counter = 1 Then
+            btn1.BackColor = Color.LightGray
+            btn2.BackColor = Color.LightGray
             btn3.BackColor = Color.Red
-            btn5.BackColor = Color.Red
+            btn4.BackColor = Color.LightGray
+            btn5.BackColor = Color.LightGray
             btn6.BackColor = Color.Red
-            btn7.BackColor = Color.Red
-
-        ElseIf Timer1.Interval = +1000 Then
-            btn3.BackColor = Color.Red
-            btn6.BackColor = Color.Red
-            btn1.BackColor = Color.Gray
-            btn2.BackColor = Color.Gray
-            btn5.BackColor = Color.Gray
-            btn6.BackColor = Color.Gray
-            btn7.BackColor = Color.Gray
+            btn7.BackColor = Color.LightGray
 
         End If
 
 
+    End Sub
 
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = TimeSpan.FromSeconds(1).TotalMilliseconds
+        Timer1.Start()
+        btn1.BackColor = Color.Red
+        btn2.BackColor = Color.Red
+        btn3.BackColor = Color.Red
+        btn4.BackColor = Color.LightGray
+        btn5.BackColor = Color.Red
+        btn6.BackColor = Color.Red
+        btn7.BackColor = Color.Red
     End Sub
 End Class
